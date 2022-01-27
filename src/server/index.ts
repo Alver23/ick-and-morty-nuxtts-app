ab6ef4233b52c817d0fab21a335f7be247f155c2
Alex Grisales <viga.23@hotmail.com>
2022-01-26 22:15:51 -0500

    feat: add api server

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,17 +3,22 @@ import express from 'express';
 import nuxtConfig from './../../nuxt.config';
 import ExpressServer from '@server/bootstrap/express-server/server';
 import HttpServer from '@server/bootstrap/http-server';
+import ApiServer from '@server/bootstrap/express-server/api';
 import config from '@server/config';
 import { normalizePort } from '@server/bootstrap/http-server/utils';
 import { Nuxt, Builder } from 'nuxt';
 
 const port: number = normalizePort(config.port as string) as number;
+const basePath = '';
 
 const start = async () => {
   try {
     const nuxt = new Nuxt(nuxtConfig);
     const expressServer = new ExpressServer(express(), port);
+    const apiServer = new ApiServer(express());
     const server = await expressServer.initialize();
+    const apiServerInstance = await apiServer.initialize();
+    server.use(`${basePath}/api`, apiServerInstance);
     const httpServer = new HttpServer(server, port);
     if (nuxtConfig.dev) {
       const builder = new Builder(nuxt);
